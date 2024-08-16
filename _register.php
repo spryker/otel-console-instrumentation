db39348f3b6a37733540b01349493a2dfb5195b1
Olha Livitchuk <olha.livitchuk@spryker.com>
2024-08-16 15:59:41 +0200

    FRW-8618 Adjustments

--- _register.php
+++ _register.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 use Spryker\Service\OtelConsoleInstrumentation\OpenTelemetry\ConsoleInstrumentation;
-use Spryker\Shared\Opentelemetry\Instrumentation\CachedInstrumentation;
-use Spryker\Shared\Opentelemetry\Request\RequestProcessor;
 
 if (extension_loaded('opentelemetry') === false) {
     error_log('The opentelemetry extension must be loaded in order to autoload the OpenTelemetry Spryker Framework auto-instrumentation', E_USER_WARNING);
@@ -12,5 +10,5 @@ if (extension_loaded('opentelemetry') === false) {
     return;
 }
 
-ConsoleInstrumentation::register(new CachedInstrumentation(), new RequestProcessor());
+ConsoleInstrumentation::register();
 
--- src/Spryker/Service/OtelConsoleInstrumentation/OpenTelemetry/ConsoleInstrumentation.php
+++ src/Spryker/Service/OtelConsoleInstrumentation/OpenTelemetry/ConsoleInstrumentation.php
@@ -16,8 +16,8 @@ use OpenTelemetry\API\Trace\StatusCode;
 use OpenTelemetry\Context\Context;
 use OpenTelemetry\Context\ContextStorageScopeInterface;
 use OpenTelemetry\SemConv\TraceAttributes;
-use Spryker\Shared\Opentelemetry\Instrumentation\CachedInstrumentationInterface;
-use Spryker\Shared\Opentelemetry\Request\RequestProcessorInterface;
+use Spryker\Shared\Opentelemetry\Instrumentation\CachedInstrumentation;
+use Spryker\Shared\Opentelemetry\Request\RequestProcessor;
 use Symfony\Component\Console\Application as ConsoleApplication;
 use Symfony\Component\HttpFoundation\Request;
 use Throwable;
@@ -51,15 +51,13 @@ class ConsoleInstrumentation
     protected const ERROR_TEXT_PLACEHOLDER = 'Error: %s in %s on line %d';
 
     /**
-     * @param \Spryker\Shared\Opentelemetry\Instrumentation\CachedInstrumentationInterface $instrumentation
-     * @param \Spryker\Shared\Opentelemetry\Request\RequestProcessorInterface $request
-     *
      * @return void
      */
-    public static function register(
-        CachedInstrumentationInterface $instrumentation,
-        RequestProcessorInterface $request
-    ): void {
+    public static function register(): void
+    {
+        $instrumentation = new CachedInstrumentation();
+        $request = new RequestProcessor();
+
         // phpcs:disable
         hook(
             class: ConsoleApplication::class,
